eada45c517573d4cb1b8cfcdf446e6582f432d0d
TienLe <trantiendev@gmail.com>
2022-08-04 15:19:42 +0700

    cpn navbar sidebar and page upload video

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,10 +6,13 @@ import { AiOutlineLogout } from 'react-icons/ai';
 import { BiSearch } from 'react-icons/bi';
 import { IoMdAdd } from 'react-icons/io';
 import { GoogleLogin, googleLogout } from '@react-oauth/google';
-
+import { createOrGetUser } from '../utils';
+import useAuthStore from '../store/authStore';
 import Logo from '../utils/tiktok-logo.png';
 
 const Navbar = () => {
+  const { userProfile, addUser, removeUser } = useAuthStore();
+  console.log(userProfile);
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
       <Link href="/">
@@ -19,10 +22,51 @@ const Navbar = () => {
             src={Logo}
             alt="logo"
             layout="fill"
-            objectFit='contain'
+            objectFit="contain"
           />
         </div>
       </Link>
+      <div>search</div>
+      <div>
+        {userProfile ? (
+         <div className='flex gap-5 md:gap-10 items-center align-center'>
+         <Link href='/upload'>
+           <button className='border-2 px-2 md:px-4 text-md font-semibold flex items-center gap-2'>
+             <IoMdAdd className='text-xl' />{' '}
+             <span className='hidden md:block'>Upload </span>
+           </button>
+         </Link>
+         {userProfile.image && (
+           <Link href={`/profile/${userProfile._id}`}>
+             <div>
+               <Image
+                 className='rounded-full cursor-pointer'
+                 src={userProfile.image}
+                 alt='user'
+                 width={40}
+                 height={40}
+               />
+             </div>
+           </Link>
+         )}
+           <button
+             type='button'
+             className='p-2 rounded-full cursor-pointer outline-none'
+             onClick={() => {
+               googleLogout();
+               removeUser();
+             }}
+           >
+              <AiOutlineLogout color='red' fontSize={21} />
+           </button>
+       </div>
+        ) : (
+          <GoogleLogin
+            onSuccess={(response) => createOrGetUser(response, addUser)}
+            onError={() => console.log('error')}
+          />
+        )}
+      </div>
     </div>
   );
 };
