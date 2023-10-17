a63ff6a2330c7974a71bfb06514ea8186c5d1032
TienLe <trantiendev@gmail.com>
2023-10-17 18:19:26 +0700

    fix image warning & deleted user

--- components/SuggestedAccounts.tsx
+++ components/SuggestedAccounts.tsx
@@ -11,51 +11,57 @@ interface IProps {
   allUsers: IUser[];
 }
 
-const SuggestedAccounts: NextPage<IProps> = ({ fetchAllUsers, allUsers, userProfile }) => {
+const SuggestedAccounts: NextPage<IProps> = ({
+  fetchAllUsers,
+  allUsers,
+  userProfile,
+}) => {
   useEffect(() => {
     fetchAllUsers();
   }, [fetchAllUsers]);
-  
+
   const users = allUsers
-    .sort(() => .5 - Math.random())
+    .sort(() => 0.5 - Math.random())
     .slice(0, allUsers.length)
-    .filter(user => user._id !== userProfile?._id);
-  
+    .filter((user) => user._id !== userProfile?._id);
+
   return (
-    <div className='xl:border-b-2 border-gray-200 pb-4'>
-    <p className='text-gray-500 font-semibold m-3 mt-4 hidden xl:block'>
-      Suggested accounts
-    </p>
-    <div>
-      {users?.slice(0, 6).map((user: IUser) => (
-        <Link href={`/profile/${user._id}`} key={user._id}>
-          <div className='flex gap-3 hover:bg-primary p-2 cursor-pointer font-semibold rounded items-center'>
-            <div className='w-8 h-8'>
-              <Image
-                width={34}
-                height={34}
-                className='rounded-full'
-                src={user?.image}
-                alt='user-profile'
-                layout='responsive'
-              />
-            </div>
+    <div className="xl:border-b-2 border-gray-200 pb-4">
+      <p className="text-gray-500 font-semibold m-3 mt-4 hidden xl:block">
+        Suggested accounts
+      </p>
+      <div>
+        {users?.slice(0, 6).map((user: IUser) => (
+          <Link href={`/profile/${user._id}`} key={user._id}>
+            <div className="flex gap-3 hover:bg-primary p-2 cursor-pointer font-semibold rounded items-center">
+              <div className="w-8 h-8">
+                {user.image && (
+                  <Image
+                    width={34}
+                    height={34}
+                    className="rounded-full"
+                    src={user?.image}
+                    alt="user-profile"
+                    layout="responsive"
+                  />
+                )}
+              </div>
 
-            <div className='hidden xl:block'>
-              <p className='flex gap-1 items-center text-md font-bold text-primary lowercase'>
-                {user.userName}
-                <GoVerified className='text-blue-400' />
-              </p>
-              <p className='capitalize text-gray-400 text-xs'>
-                {user.userName}
-              </p>
+              <div className="hidden xl:block">
+                <p className="flex gap-1 items-center text-md font-bold text-primary lowercase">
+                  {user.userName}
+                  <GoVerified className="text-blue-400" />
+                </p>
+                <p className="capitalize text-gray-400 text-xs">
+                  {user.userName}
+                </p>
+              </div>
             </div>
-          </div>
-        </Link>
-      ))}
+          </Link>
+        ))}
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default SuggestedAccounts
+export default SuggestedAccounts;
